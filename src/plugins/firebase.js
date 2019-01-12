f8c1dcc69335cb1b4bb04f9b32e4b83d7fc179be
Andrei Stefanie <andrei.stefanie@gmail.com>
2019-01-12 15:23:40 +0200

    Enable offline firestore support; moved to env variables

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,24 +1,35 @@
 import Vue from 'vue';
 import VueFire from 'vuefire';
-import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyCY-9mPf0I_91juKitXcJFlYCtHRjzIw5Q',
+  apiKey: process.env.VUE_APP_FIREBASE_KEY,
   authDomain: 'slimlist-19a19.firebaseapp.com',
   databaseURL: 'https://slimlist-19a19.firebaseio.com',
   projectId: 'slimlist-19a19'
 };
 
-const app = initializeApp(config);
+const app = firebase.initializeApp(config);
 
 Vue.use(VueFire);
 
 export default app;
 
 const db = app.firestore();
+
 db.settings({
   timestampsInSnapshots: true
+});
+
+db.enablePersistence().catch(err => {
+  if (err.code == 'failed-precondition') {
+    // Multiple tabs open, persistence can only be enabled
+    // in one tab at a a time.
+  } else if (err.code == 'unimplemented') {
+    // The current browser does not support all of the
+    // features required to enable persistence
+  }
 });
 
 export { db };
